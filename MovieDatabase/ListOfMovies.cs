653d97fa0452dce28e80f7b503a969e9cb804182
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-19 14:11:51 -0400

    Added a print categories method to listOfMovies

--- MovieDatabase/ListOfMovies.cs
+++ MovieDatabase/ListOfMovies.cs
@@ -4,25 +4,56 @@ namespace MovieDatabase
 {
     internal class ListOfMovies
     {
+        public static List<string> categories = new() { "Action", "Comedy", "Drama", "Horror" };
+
         public static List<Movie> movieList = new()
             {
-                new Movie("bob", "Action"),
-                new Movie("tom", "Comedy"),
+                new Movie("10,000 BC", "Action"),
+                new Movie("Apollo 13", "Drama"),
+                new Movie("Catch Me If You Can", "Drama"),
+                new Movie("Mrs. Doubtfire", "Comedy"),
+                new Movie("The Talented Mr. Ripley", "Drama"),
+                new Movie("Groundhog Day", "Comedy"),
+                new Movie("Paycheck", "Action"),
+                new Movie("Cellular", "Action"),
+                new Movie("Speed", "Action"),
+                new Movie("Die Hard", "Action"),
+                new Movie("Paranormal Activity", "Horror"),
+                new Movie("The Exorcist", "Horror"),
+                new Movie("Anchorman", "Comedy"),
+                new Movie("Office Space", "Comedy"),
+                new Movie("An American Girl Holiday", "Drama"),
+                new Movie("Titanic", "Drama"),
+                new Movie("Edward Scissorhands", "Drama"),
+                new Movie("The Black Phone", "Horror"),
+                new Movie("Incantation", "Horror"),
             };
 
         public static void PrintMovieList()
         {
             int movieNumber = 1;
-            movieList.ToList().ForEach(i => WriteLine($"{movieNumber++,-2}: {i.GetTitle,-12}{i.GetCategory}"));
+            Console.WriteLine("List of all movies.\n");
+            movieList.OrderBy(x => x.GetCategory)
+                .ThenBy(x => x.GetTitle)
+                .ToList()
+                .ForEach(i => WriteLine($"{movieNumber++,-2}: {i.GetTitle,-30} {i.GetCategory}"));
         }
 
         public static void PrintMovieCategory(string cat)
         {
-            int movieNumber = 1;
-            Console.WriteLine($"List of {cat} movies.");
-            movieList.Where(s => s.GetCategory == cat)
+            Console.WriteLine($"List of {cat} movies.\n");
+            movieList.OrderBy(x => x.GetTitle)
+                .Where(s => s.GetCategory == cat)
                 .ToList()
-                .ForEach(i => Console.WriteLine($"{movieNumber++,-2}: { i.GetTitle,-12}"));
+                .ForEach(i => Console.WriteLine(i.GetTitle));
+        }
+
+        public static void PrintCategories()
+        {
+            Console.WriteLine("List of movie categories.\n");
+            categories.OrderBy(x => x)
+                .ToList()
+                .ForEach(i => Console.WriteLine(i));
         }
     }
 }
--- MovieDatabase/Program.cs
+++ MovieDatabase/Program.cs
@@ -6,12 +6,17 @@ namespace MovieDatabase
     {
         static void Main()
         {
-
+            PrintCategories();
+            Console.WriteLine();
             PrintMovieList();
             Console.WriteLine();
             PrintMovieCategory("Action");
             Console.WriteLine();
             PrintMovieCategory("Comedy");
+            Console.WriteLine();
+            PrintMovieCategory("Drama");
+            Console.WriteLine();
+            PrintMovieCategory("Horror");
             //movieList.Where(s => s.GetCategory == "Action")
             //    .ToList()
             //    .ForEach(w => Console.WriteLine(w.GetTitle));
