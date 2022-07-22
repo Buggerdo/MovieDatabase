c8ef2bf0b05db3fe440ee0ed84e2153486887c86
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-22 13:28:22 -0400

    Changed welcome message and added else for more then one match on input

--- MovieDatabase/MainMenu.cs
+++ MovieDatabase/MainMenu.cs
@@ -6,14 +6,17 @@ namespace MovieDatabase
     {
         public static void StartMainMenu()
         {
+            Console.Clear();
+            Console.WriteLine("Welcome to the Movie List Application!");
+            Console.WriteLine();
             PrintCategories();
-
             Console.WriteLine();
             Console.WriteLine("Enter a movie category you would like to see.");
             Console.WriteLine("Or you can select a category by index numbers.");
             Console.WriteLine($"There are {movieList.Count} movies in this list.");
             Console.WriteLine();
             string input = Console.ReadLine().ToLower().Trim();
+
             if(input != string.Empty && categories.Where(c => c.ToLower().Trim().StartsWith(input)).Any())
             {
                 string[] choice = categories.Where(c => c.ToLower().Trim().StartsWith(input)).ToArray();
@@ -24,9 +27,13 @@ namespace MovieDatabase
                         if(choice[0] == cat)
                         {
                             PrintMovieCategory(cat);
-                            //break;
                         }
                     }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Sorry, there was more then one entry that matched your search.");
                 }
             }
             else if(int.TryParse(input, out int index) && --index < categories.Count)
